90945f2c16332a7085d6ae6e4bb74fae80c56a19
baosphoa0403 <baosphoa0403@gmail.com>
2021-07-20 12:56:58 +0700

    add first

---
+++ src/Config/index.ts
@@ -0,0 +1,10 @@
+import { PATH_NAME } from './pathName';
+import { USER_ROLE } from './userRole';
+
+export const THEMES = {
+  LIGHT: 'light',
+  DARK: 'dark',
+};
+
+
+export { PATH_NAME, USER_ROLE };
---
+++ src/Config/pathName.ts
@@ -0,0 +1,9 @@
+export const PATH_NAME = {
+    ROOT: '/',
+    ERROR_404: '/404',
+    LOGIN: '/login',
+    PRODUCT: '/product',
+    PRODUCT_LIST: '/product/list',
+    PRODUCT_ADD: '/product/add',
+    USERS: '/users',
+};
