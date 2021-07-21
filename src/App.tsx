647d7af3977b1a58a36028a7a2ec886cc485bf8d
LamNguyen <9.4ngoclam@gmail.com>
2021-07-22 00:11:13 +0700

    to be continued

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import MainLayout from './Layout/MainLayout';
+import React from "react";
+import "./App.css";
+import MainLayout from "./Layout/MainLayout";
 
 function App() {
   return (
     <div className="App">
-      <MainLayout/>
+      <MainLayout />
     </div>
   );
 }
--- src/Layout/Navbar/Navbar.tsx
+++ src/Layout/Navbar/Navbar.tsx
@@ -1,23 +1,63 @@
+import {
+  AppBar,
+  Button,
+  IconButton,
+  InputBase,
+  Link,
+  Toolbar,
+  Typography,
+} from "@material-ui/core";
+import React from "react";
+import { useStyles } from "./Style";
+import EcoIcon from "@material-ui/icons/Eco";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 
-import { AppBar, Button, IconButton, Toolbar, Typography } from '@material-ui/core';
-import React from 'react'
-import {useStyles} from "./Style";
-import MenuIcon from '@material-ui/icons/Menu';
 export default function Navbar() {
-    const classes = useStyles();
-    return (
-        <div className={classes.root}>
-        <AppBar position="static">
-          <Toolbar>
-            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" className={classes.title}>
-              News
-            </Typography>
-            <Button color="inherit">Login</Button>
-          </Toolbar>
-        </AppBar>
-      </div>
-    )
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.header}>
+        <Toolbar>
+          <EcoIcon />
+          <Typography className={classes.menuList}>
+            <Link href="#" className={classes.menuItem} underline="none">
+              Men
+            </Link>
+            <Link href="#" className={classes.menuItem} underline="none">
+              Women
+            </Link>
+            <Link href="#" className={classes.menuItem} underline="none">
+              Kids
+            </Link>
+            <Link href="#" className={classes.menuItem} underline="none">
+              Customise
+            </Link>
+            <Link href="#" className={classes.menuItem} underline="none">
+              Sale
+            </Link>
+            <Link href="#" className={classes.menuItem} underline="none">
+              SNKRS
+            </Link>
+          </Typography>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="Search"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+            />
+          </div>
+          <IconButton>
+            <ShoppingCartIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
