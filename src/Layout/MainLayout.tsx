d9bbf4cfe39bea9a0fa23df007889e7ebf136d77
baosphoa0403 <baosphoa0403@gmail.com>
2021-07-24 10:40:15 +0700

    add code

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Carousel from "./Carousel/Carousel";
-import NavBar from "./Navbar/NavBar";
+import NavBar from "./Navbar/Navbar";
+// import NavBar from "./Navbar/NavBar";
 
 export default function MainLayout() {
   return (
