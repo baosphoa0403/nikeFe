31247504acaf6c50684ff971e5b510a4196586bb
LamNguyen <9.4ngoclam@gmail.com>
2021-07-30 20:16:06 +0700

    display list item

---
+++ src/Pages/Admin/AdminPage.tsx
@@ -0,0 +1,12 @@
+import React, { Fragment } from "react";
+import AdminMenu from "../../Layout/Admin/AdminMenu";
+
+function AdminPage() {
+  return (
+    <Fragment>
+      <AdminMenu />
+    </Fragment>
+  );
+}
+
+export default AdminPage;
