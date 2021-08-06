22efcc4072a2dadaa9ebeaab1a2fca290de087df
baosphoa0403 <baosphoa0403@gmail.com>
2021-08-06 11:29:22 +0700

    Add code

--- src/Redux/moduleDemo/action/Action.tsx
+++ src/Redux/moduleDemo/action/Action.tsx
@@ -1,13 +1,17 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const callAPI = () => axios({
-  method: 'GET',
-  url: 'https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01',
-});
+export const callAPI = () =>
+  axios({
+    method: 'GET',
+    url: 'https://movie0706.cybersoft.edu.vn/api/QuanLyPhim/LayDanhSachPhim?maNhom=GP01',
+  });
 
-export const getAll: any = createAsyncThunk('listMovie/getAll', async (params, thunkAPI) => {
-  //    thunkAPI.dispatch() để bắn lên store
-  const listMovie = await callAPI();
-  return listMovie;
-});
+export const getAll: any = createAsyncThunk(
+  'listMovie/getAll',
+  async (params, thunkAPI) => {
+    //    thunkAPI.dispatch() để bắn lên store
+    const listMovie = await callAPI();
+    return listMovie.data;
+  }
+);
