8234fcfc78e5509ae8ffafbb60f8dbb7522e8d22
baosphoa0403 <baosphoa0403@gmail.com>
2021-08-06 10:31:26 +0700

    remove navbar vs header

--- src/Layout/MainLayout.tsx
+++ src/Layout/MainLayout.tsx
@@ -1,20 +1,18 @@
-import React, { Fragment } from "react";
-import Counter from "../Component/Counter";
-import Carousel from "./Carousel/Carousel";
-import Footer from "./Footer/Footer";
-import ShowProductsAndFilter from "./ListProducts/ShowProductsAndFilter";
-import NavBar from "./Navbar/Navbar";
+import React, { Fragment } from 'react';
+import Counter from '../Component/Counter';
+import Carousel from './Carousel/Carousel';
+import Footer from './Footer/Footer';
+import ShowProductsAndFilter from './ListProducts/ShowProductsAndFilter';
+import NavBar from './Navbar/Navbar';
 
 // import NavBar from "./Navbar/NavBar";
 
 export default function MainLayout() {
   return (
     <Fragment>
-      <NavBar />
       <Carousel />
       <ShowProductsAndFilter />
       <Counter />
-      <Footer />
     </Fragment>
   );
 }
